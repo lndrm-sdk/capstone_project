38d52fe524d63462835b515cd0363284abdb3892
lndrm-sdk <lundrim.sadiku@genpact.com>
2026-02-18 22:00:01 +0000

    ..

--- notebooks/bronze.py
+++ notebooks/bronze.py
@@ -1,4 +1,10 @@
 # Databricks notebook source
+catalog = dbutils.widgets.get("catalog")
+print("Using catalog:", catalog)
+
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC CREATE OR REPLACE TABLE bronze.customer_raw (
 # MAGIC     customer_id STRING,
